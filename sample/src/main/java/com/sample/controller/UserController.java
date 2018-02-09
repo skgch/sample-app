3feff03b155e0890edddb6e0ccbf4b4f0eae13d8
skgch <m.sakaguchi04@gmail.com>
2018-02-09 18:54:09 +0900

    Add error messages and success flash when sign up

--- sample/src/main/java/com/sample/controller/UserController.java
+++ sample/src/main/java/com/sample/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sample.dto.SignUpFormDto;
 import com.sample.entity.User;
@@ -31,25 +33,17 @@ public class UserController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public ModelAndView create(@ModelAttribute("formModel") @Validated SignUpFormDto formDto,
-            BindingResult result, ModelAndView mav) {
-        mav.setViewName(TEMPLATE_DIR + "/signUp");
-        mav.addObject("title", "Sign Up");
+    public String create(@ModelAttribute("formModel") @Validated SignUpFormDto formDto,
+            BindingResult result, Model model, RedirectAttributes redirectAttrs) {
+		if (result.hasErrors()) {
+			model.addAttribute("formModel", formDto);
+			return TEMPLATE_DIR + "/signUp";
+		}
 
-        if(result.hasErrors()) {
-            mav.addObject("formDto", formDto);
-            return mav;
-            //return new ModelAndView("redirect:/signup");
-        }
-
-        else {
-            service.save(formDto.getUsername(), formDto.getEmail(), formDto.getPassword());
-            // test
-            Iterable<User> users = service.findAll();
-            mav.addObject("users", users);
-            return mav;
-        }
-
+		User user = service.save(formDto.getUsername(), formDto.getEmail(), formDto.getPassword());
+		redirectAttrs.addFlashAttribute("flash", "Welcome to the Sample App!");
+		// TODO Redirect to user page insted of home page
+		return "redirect:/";
     }
 
     @RequestMapping(value = "user/{id}")

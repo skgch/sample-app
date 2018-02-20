82d520132fbd2699ca9363100e01753ec3bfc1c7
skgch <m.sakaguchi04@gmail.com>
2018-02-21 00:28:18 +0900

    Add remember me(in memory, insecure)

--- sample/src/main/java/com/sample/config/WebSecurityConfig.java
+++ sample/src/main/java/com/sample/config/WebSecurityConfig.java
@@ -40,7 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login?error")
                 .defaultSuccessUrl("/default")
                 .permitAll()
-                .and();
+                .and()
+                .rememberMe();
 
         http
                 .logout()
